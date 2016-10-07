f3d819cef5f03e2899d9e2969c70136a57deb4f2
Reimar75 <me@reimar.org>
2016-10-07 11:21:56 +0200

    Use riddle title for headline if no result data (e.g. rankedlist)

--- _content-default.php
+++ _content-default.php
@@ -7,52 +7,55 @@
     </div>
 
     <div class="jumbotron text-center">
-        <h1><?php echo $riddle->riddle->title; ?></h1>
-        <?php if (isset($riddle->riddle->description)): ?>
-            <p class="lead"><?php echo $riddle->riddle->description; ?></p>
-        <?php endif; ?>
+        <h1><?php
+            echo $riddle->resultData->title ?
+                    $riddle->resultData->title :
+                    $riddle->riddle->title;
+            ?>
+        </h1>
+        <p class="lead"><?php echo $riddle->resultData->description; ?></p>
     </div>   
 
-    <?php if ($riddle->answers): ?>
+<?php if ($riddle->answers): ?>
         <div class="page-header text-center">
             <h2>Questions and answers</h2>
         </div>
         <div class="list-group">
             <?php $numberAnswers = count($riddle->answers); ?>
-            <?php foreach ($riddle->answers as $_k => $_answer): ?>
-                <?php $_count = ($_k + 1) . '/' . $numberAnswers; ?>
+                <?php foreach ($riddle->answers as $_k => $_answer): ?>
+                    <?php $_count = ($_k + 1) . '/' . $numberAnswers; ?>
                 <a href="#" class="list-group-item">
-                    <?php if ('quiz' === $riddle->riddle->type): ?>                    
+                        <?php if ('quiz' === $riddle->riddle->type): ?>                    
                         <span class="badge" style="background:<?php echo $_answer->correct ? 'green' : 'red'; ?>">
-                            <?php echo $_answer->correct ? 'Correct' : 'Wrong'; ?>
+                        <?php echo $_answer->correct ? 'Correct' : 'Wrong'; ?>
                         </span>
-                    <?php endif; ?>
+                        <?php endif; ?>
                     <h4 class="list-group-item-heading">
                         <?php echo $_k + 1; ?>. <?php echo $_answer->question; ?>
                     </h4>
                     <p class="list-group-item-text">
-                        <?php echo $_answer->answer; ?>
+                <?php echo $_answer->answer; ?>
                     </p>                    
                 </a>
-            <?php endforeach; ?>
+        <?php endforeach; ?>
         </div>
-    <?php endif; ?>
+<?php endif; ?>
 
     <div class="page-header text-center">
         <h2>Lead data</h2>
     </div>
-    <?php if ($riddle->lead): ?>
+        <?php if ($riddle->lead): ?>
         <div class="list-group">       
-            <?php foreach ($riddle->lead as $_field => $_value): ?>
+                    <?php foreach ($riddle->lead as $_field => $_value): ?>
                 <a href="#" class="list-group-item">
                     <h4 class="list-group-item-heading">
                         <?php echo $_field; ?>
                     </h4>
                     <p class="list-group-item-text">
-                        <?php echo $_value; ?>
+                <?php echo $_value; ?>
                     </p> 
                 </a>
-            <?php endforeach; ?>
+        <?php endforeach; ?>
         </div>
     <?php else: ?>
         <p><i>No lead data</i></p>
